f7e5bb531550f5b5f4da258b02e05aac4ef9b34a
Diogo <diogo.marcondes2004@gmail.com>
2025-02-08 16:04:26 -0300

    First commmit with real project

---
+++ src/components/Nav.tsx
@@ -0,0 +1,19 @@
+import "@/app/styles/nav.css"
+
+export const Nav = () => {
+    return (
+        <nav className="nav-bar">
+            <ul className="menu-about">
+                <li className="menu-item"><a href="#">Diogo Marcondes</a></li>
+                <li className="menu-item">FullStack Developer</li>
+            </ul>
+            <ul className="menu-links">
+                <li className="menu-item"><a href="#">Home</a></li>
+                <li className="menu-item"><a href="#">Projects</a></li>
+                <li className="menu-item"><a href="#">About me</a></li>
+                <li className="menu-item"><a href="#">Knowledge</a></li>
+                <li className="menu-item"><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
